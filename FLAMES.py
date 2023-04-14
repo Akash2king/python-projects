ace3cf6a7cfc593ab9025b0cee278683db1c94c1
Logesh <89137121+Akash2king@users.noreply.github.com>
2023-04-14 11:17:42 +0530

    Declare seperate process as a function
    This will be more use full

--- FLAMES.py
+++ FLAMES.py
@@ -1,17 +1,20 @@
 a=list(input("Enter a name :")) 
 b=list(input("Enterva name :")) 
-i=j=0
-while (len(a)>i):
-  while (len(b)>j):
-    if (a[i]==b[j]):
-      del a[i]
-      del b[j]
-      i=j=0
-    else:
-      j+=1
-  j=0
-  i+=1
-print(a, b, sep="\n")
-c=a+b
-num=len(c)
-print ( num) 
+def sep(a, b):
+	i=j=0
+	while (len(a)>i):
+		  while (len(b)>j):
+		  	  if (a[i]==b[j]):
+		  	  	del a[i]
+		  	  	del b[j]
+		  	  	i=j=0
+		  	  else:
+		  	  	j+=1
+		  j=0
+		  i+=1
+	print(a, b, sep="\n")
+	c=a+b
+	num=len(c)
+	print ( num) 
+sep(a, b)
+#
